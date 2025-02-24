80eb50dcba81084a76351f103f7e0e5cadc3f9fd
Jules Voyer <jules.voyer@gmail.com>
2025-02-24 11:14:00 +0100

    zone 4 points

--- map.py
+++ map.py
@@ -3,11 +3,12 @@ from djitellopy import Tello
 
 import time
 if __name__ == "__main__":
-    height = [0,0]
+    height = [[0,0],
+              [0,0]]
     tello = Tello()
     tello.connect()
 
-    #start position is (0,0)
+    #start position is (0,-1)
     tello.enable_mission_pads()
 
     #takeoff
@@ -19,38 +20,57 @@ if __name__ == "__main__":
     print("after takeoff")
     print(tello.get_height())
 
-    tello.move_up(20)
+    tello.move_up(50)
 
     time.sleep(5)
 
     print("after move up")
     print(tello.get_height())
 
+    #move to (0,0)
+    tello.move_forward(50)
 
-    tello.move_forward(25)
+    time.sleep(5)
+    print("after move forward")
+    height[0][0] = tello.get_height()
+    print(height[0][0])
+
+    #move to (1,0)
+    tello.move_right(50)
+
     time.sleep(5)
 
-    print("after move forward 1")
-    print(tello.get_height())
+
+    print("after move right")
+    height[1][0] = tello.get_height()
+    print(height[1][0])
 
 
-    tello.move_forward(25)
+    #move to (1,1)
+
+    tello.move_forward(50)
+
     time.sleep(5)
 
-    print("after move forward 2")
-    print(tello.get_height())
+
+    print("after move forward")
+    height[1][1] = tello.get_height()
+    print(height[1][1])
 
 
-    tello.flip("b")
+    #move to (0,1)
+
+    tello.move_left(50)
+
     time.sleep(5)
 
-    print("after flip")
+    print("after move left")
+    height[0][1] = tello.get_height()
 
-    print(tello.get_height())
 
 
     tello.rotate_clockwise(180)
-    tello.move_forward(50)
+    tello.move_forward(100)
     #Original orientation
     tello.rotate_clockwise(180)
     tello.land()
